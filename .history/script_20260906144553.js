ef2565b06dbd9ef3520af04f59ad541460f83298
Gaurav124937 <singhxdgaurav@gmail.com>
2026-09-10 20:46:44 +0530

    added delete button to remove expense from html as well as expenses array

---
+++ .history/script_20260906144553.js
@@ -0,0 +1,78 @@
+const addExpenseButton = document.querySelector(".add-expense-button");
+
+const expenseForm = document.querySelector(".expense-form");
+
+addExpenseButton.addEventListener("click", function showExpenseForm() {
+  expenseForm.classList.toggle("form-hidden");
+});
+
+const expenses = [];
+const expenseList = document.querySelector(".expense-list");
+
+expenseForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const amount = Number(document.querySelector("#expense").value);
+  const descript = document.querySelector("#description").value;
+  const category = document.querySelector("#category").value;
+  const date = document.querySelector("#date").value;
+  const expense = {
+    id: crypto.randomUUID(),
+    expense: amount,
+    description: descript,
+    category: category,
+    date: date,
+  };
+  expenses.push(expense);
+  const expenseArrayString = JSON.stringify(expenses);
+  localStorage.setItem('array', ex);
+  console.log(expenses);
+
+  const expenseElement = document.createElement("div");
+  expenseElement.id = expense.id;
+
+  expenseElement.innerHTML = `
+    <p>${expense.description}</p>
+    <p>₹${expense.expense}</p>
+    <p>${expense.category}</p>
+  <p>${expense.date}</p>
+  <button class="delete-expense-button">Delete</button>
+
+`;
+  expenseList.appendChild(expenseElement);
+
+  let totalValue = totalExpense();
+
+  const totalAmount = document.querySelector(".total-expense");
+  totalAmount.innerHTML = `<p>TOTAL EXPENSE: ${totalValue}</p>`;
+
+  const deleteExpense = expenseElement.querySelector(".delete-expense-button");
+  deleteExpense.addEventListener("click", function () {
+    expenseElement.remove();
+    const index = expenses.findIndex(
+      (expense) => expense.id === expenseElement.id,
+    );
+    if (index !== -1) {
+      expenses.splice(index, 1);
+    }
+    localStorage.setItem('array', expenses);
+    totalValue = totalExpense();
+    totalAmount.innerHTML = `<p>TOTAL EXPENSE: ${totalValue}</p>`;
+    console.log(expenses);
+  });
+
+
+  
+})
+
+  
+  
+
+
+
+function totalExpense() {
+  const totalExpense = expenses.reduce((total, expense) => {
+    return total + expense.expense;
+  }, 0);
+  return totalExpense;
+}
---
+++ .history/script_20260908130708.js
@@ -0,0 +1,122 @@
+const addExpenseButton = document.querySelector(".add-expense-button");
+
+const expenseForm = document.querySelector(".expense-form");
+
+addExpenseButton.addEventListener("click", function showExpenseForm() {
+  expenseForm.classList.toggle("form-hidden");
+});
+
+const expenses = [];
+getData();
+
+expenses.forEach((expense) => {
+  const expenseList = document.querySelector(".expense-list");
+  const expenseElement = document.createElement("div");
+  expenseElement.id= expense.id;
+  expenseElement.className = "expense-display-card";
+  expenseElement.innerHTML = `
+    <p>${expense.description}</p>
+    <p>₹${expense.expense}</p>
+    <p>${expense.category}</p>
+    <p>${expense.date}</p>
+    <button class="delete-card">delete</button>
+  `;
+  expenseList.appendChild(expenseElement);
+
+const deleteCard = expenseElement.querySelector(".delete-card");
+  deleteCard.addEventListener("click", function () {
+    expenseElement.remove();
+    const index = expenses.findIndex(
+      (expense) => expense.id === expenseElement.id,
+    );
+    if (index !== -1) {
+      expenses.splice(index, 1);
+    }
+    storingData();
+  });
+   
+});
+
+let total = totalExpense();
+const totalSub = document.querySelector(".total-value");
+  totalSub.innerHTML = `<p>TOTAL EXPENSE: ${total}</p>`;
+
+
+
+
+const expenseListSubmit = document.querySelector(".expense-list-submit");
+
+expenseForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const amount = Number(document.querySelector("#expense").value);
+  const descript = document.querySelector("#description").value;
+  const category = document.querySelector("#category").value;
+  const date = document.querySelector("#date").value;
+  const expense = {
+    id: crypto.randomUUID(),
+    expense: amount,
+    description: descript,
+    category: category,
+    date: date,
+  };
+  expenses.push(expense);
+
+  storingData();
+
+  const expenseElement = document.createElement("div");
+  expenseElement.id = expense.id;
+
+  expenseElement.innerHTML = `
+    <p>${expense.description}</p>
+    <p>₹${expense.expense}</p>
+    <p>${expense.category}</p>
+  <p>${expense.date}</p>
+  <button class="delete-expense-button">Delete</button>
+
+`;
+  expenseListSubmit.appendChild(expenseElement);
+
+  let totalValue = totalExpense();
+
+  const totalAmount = document.querySelector(".total-expense");
+  totalAmount.innerHTML = `<p>TOTAL EXPENSE: ${totalValue}</p>`;
+
+  const deleteExpense = expenseElement.querySelector(".delete-expense-button");
+  deleteExpense.addEventListener("click", function () {
+    expenseElement.remove();
+    const index = expenses.findIndex(
+      (expense) => expense.id === expenseElement.id,
+    );
+    if (index !== -1) {
+      expenses.splice(index, 1);
+    }
+
+    storingData();
+
+    totalValue = totalExpense();
+    totalAmount.innerHTML = `<p>TOTAL EXPENSE: ${totalValue}</p>`;
+  });
+});
+
+expenseForm.reset();
+
+function totalExpense() { 
+  const totalExpense = expenses.reduce((total, expense) => {
+    return total + expense.expense;
+  }, 0);
+  return totalExpense;
+}
+
+function storingData() {
+  const expenseArrayString = JSON.stringify(expenses);
+  localStorage.setItem("array", expenseArrayString);
+}
+
+function getData() {
+  const expensesArray = JSON.parse(localStorage.getItem("array"));
+  if (expensesArray !== null) {
+    expenses.push(...expensesArray);
+  }
+
+  console.log(expensesArray);
+}
